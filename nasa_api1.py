f2e613cc9c065c0d17900f8da54ac69b908bde18
sarita <saritakatherinegomez@gmail.com>
2025-08-22 21:33:02 -0500

    ML – Challenge 22

--- nasa_api1.py
+++ nasa_api1.py
@@ -1,27 +1,44 @@
 '''
-Script description:
-get and read data from nasa API about space 
-Nasa API: https://api.nasa.gov/
-dev: sarita gomez
-https://api.nasa.gov/neo/rest/v1/neo/3726712?api_key={A}
+Script description: Get and read data from NASA API about space
+NASA API: https://api.nasa.gov/
+Dev: Sarita.GM
+https://api.nasa.gov/neo/rest/v1/neo/3726712?api_key={API_KEY_HERE}
 '''
+
 import os
 import requests
 
 os.system('clear')
-def get_apinasa_data(api_key):
-    print("::: NASA INFORMTAION :::")
+
+
+def get_nasa_data(api_key):
+    print("::: NASA INFORMATION :::")
     url = f"https://api.nasa.gov/neo/rest/v1/neo/3726712?api_key={api_key}"
 
-
     try:
-        #API request
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
+
+        # Extraer información 
+        name = data.get("name", "No disponible")
+        magnitude = data.get("absolute_magnitude_h", "No disponible")
+        diameter_km = data["estimated_diameter"]["kilometers"]["estimated_diameter_max"]
+        diameter_ft = data["estimated_diameter"]["feet"]["estimated_diameter_max"]
+
+        # Mostrar en consola
+        print(f"Nombre del cometa: {name}")
+        print(f"Magnitud absoluta h: {magnitude}")
+        print(f"Diámetro máximo estimado (KM): {diameter_km}")
+        print(f"Diámetro máximo estimado (FT): {diameter_ft}")
+
     except requests.exceptions.RequestException as e:
-        print(f"request error:  {e}")
+        print(f"Request error: {e}")
+    except KeyError as ke:
+        print(f"Error de clave en JSON: {ke}")
 
+
+# API Key de NASA
 NASA_API_KEY = "Ep1iocabsI4FpqCeyfdTh9GsLmtazY9gBgYLVOeW"
-get_nasa_data(NASA_API_KEY)
 
+get_nasa_data(NASA_API_KEY)
